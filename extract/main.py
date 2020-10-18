d3253e2cf586a1c98629bd7e18830c9629627cc6
Oscar Palomino <ing.oscarp1@gmail.com>
2020-10-17 22:33:11 -0500

    Function that extract the category links and function that extract news for each category completed

--- extract/main.py
+++ extract/main.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 config = config
 
 def category_links_extraction(host, iterator):
-    ''' Returns a list of the category links '''
+    ''' Function that returns a list of the category links '''
     logger.info(f'Extracting category list for {host}')
     # Variables definition
     categories_selector = config()['news_sites'][iterator]['categories']
@@ -31,25 +31,44 @@ def category_links_extraction(host, iterator):
 
     return categories_links
 
-url = config()['news_sites'][0]['url']
 
-links = category_links_extraction(url, 0)
-print(links)
-for link in links:
-    logger.info(f'Starting links extraction for {link}')
-    try:
-        category_request = requests.get(link)
-        category_soup = BeautifulSoup(category_request, 'lxml')
-        # type(category_soup)
-    except Exception as e:
-        logger.info(f'Error with server {link}')
-        break
-    try:
-        links_container = category_soup.find('section', attrs = {'id':'r01-c01'}).find_all('article')
-        # type(links_container)
- 
-# if __name__ == '__main__':
-#     for i in range(len(config())):
-#         host = config()['news_sites'][i]['url']
-#         logger.info(f'Begining scraper for {host}')
-#         print(category_links_extraction(host, i))
+def articles_links_extraction(url_list, iterator):
+    ''' Function that extracts the urls for each category, and returns it in a list. '''
+    article_container_selector = config()['news_sites'][iterator]['articles']
+    article_container_selector_attributes = config()['news_sites'][iterator]['articles_attributes']
+    article_selector = config()['news_sites'][iterator]['find_articles']
+    article_selector_attributes = config()['news_sites'][iterator]['find_articles_attributes']
+    article_list_tmp = []
+    article_list = []
+    for link in url_list:
+        logger.info(f'Starting links extraction for {link}')
+        try:
+            category_request = requests.get(link)
+            category_soup = BeautifulSoup(category_request.text, 'lxml')
+            # type(category_soup)
+        except Exception as e:
+            logger.info(f'Error with server {link}')
+            
+        try:
+            articles_container = category_soup.find(article_container_selector, attrs = article_container_selector_attributes).find_all(article_selector, attrs = article_selector_attributes)
+            for article in articles_container:
+                article_list_tmp.append(host + article.a.get('href'))
+        except Exception as e:
+            logger.info(f'There is no article.')
+
+    # Deleting repeated articles 
+    for i in article_list_tmp:
+        if i not in article_list:
+            article_list.append(i)
+
+    return article_list
+
+
+if __name__ == '__main__':
+    for i in range(len(config())):
+        host = config()['news_sites'][i]['url']
+        logger.info(f'Begining scraper for {host}')
+        categories = category_links_extraction(host, i)
+        articles_links = articles_links_extraction(categories, i)
+        print(articles_links)
+        print(len(articles_links))
