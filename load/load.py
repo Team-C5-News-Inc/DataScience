d56947d2298e1d8c7ff62dc0e4e03c853e8acfd6
Oscar Palomino <ing.oscarp1@gmail.com>
2020-10-28 16:09:15 -0500

    first scrapper

--- load/load.py
+++ load/load.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
-db = client['news_inc']
+db = client['news_db']
 logger.info(f'Conecting to DB {db.name}.')
 data_articles = []
 data_categories = []
@@ -66,33 +66,33 @@ df_articles = _cleaning_vanguardia_images(df_articles)
 df_categories = pd.read_csv('clean_categories.csv')
 
 logger.info(f'Accessing to collections.')
-collection_articles = db['articles']
+collection_articles = db['news']
 collection_categories = db['categories']
 
-# logger.info(f'Parsing article data.')
-# for article in range(len(df_articles)):
-#     data_articles.append({
-#         'title': df_articles['title'][article],
-#         'subtitle': df_articles['subtitle'][article],
-#         'images': df_articles['images'][article],
-#         'body': df_articles['body'][article],
-#         'tags': df_articles['tags'][article],
-#         'author': df_articles['author'][article],
-#         'host': df_articles['host'][article],
-#         'news_url': df_articles['news_url'][article],
-#         'publication_date': df_articles['publication_date'][article],
-#         'category': df_articles['category_long'][article]
-#     })
-# logger.info(f'Parsing Category data.')
-# for category in range(len(df_categories)):
-#     data_categories.append({'categories':df_categories['categories'][category]})
+logger.info(f'Parsing article data.')
+for article in range(len(df_articles)):
+    data_articles.append({
+        'title': df_articles['title'][article],
+        'subtitle': df_articles['subtitle'][article],
+        'images': df_articles['images'][article],
+        'body': df_articles['body'][article],
+        'tags': df_articles['tags'][article],
+        'author': df_articles['author'][article],
+        'host': df_articles['host'][article],
+        'news_url': df_articles['news_url'][article],
+        'publication_date': df_articles['publication_date'][article],
+        'category': df_articles['category_long'][article]
+    })
+logger.info(f'Parsing Category data.')
+for category in range(len(df_categories)):
+    data_categories.append({'categories':df_categories['categories'][category]})
 
 
-# logger.info(f'Saving data into database.')
-# collection_articles.insert_many(data_articles)
-# collection_categories.insert_many(data_categories)
+logger.info(f'Saving data into database.')
+collection_articles.insert_many(data_articles)
+collection_categories.insert_many(data_categories)
 
-# logger.info(f'Closing database {db.name}.')
+logger.info(f'Closing database {db.name}.')
 
 client.close()
 
